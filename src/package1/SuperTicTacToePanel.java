37e80498660497eaed1580debfa08fa57e03ab36
Remy <55114184+remane-bit@users.noreply.github.com>
2019-09-27 17:36:43 -0400

    Initial commit

---
+++ src/package1/SuperTicTacToePanel.java
@@ -0,0 +1,16 @@
+package package1;
+
+import javax.swing.*;
+
+public class SuperTicTacToePanel {
+
+    private JButton[][] board;
+    private Cell[][] iBoard;
+    private JButton quitButton;
+    private ImageIcon xIcon;
+    private ImageIcon oIcon;
+    private ImageIcon emptyIcon;
+
+    private SuperTicTacToeGame game;
+
+}
