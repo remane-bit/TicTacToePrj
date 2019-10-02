94e32873373c38374ced29ec173adff37f392f01
mzig0203 <zieglerm@mail.gvsu.edu>
2019-10-02 19:35:16 -0400

    Added the JOption Panel with multiple inputs

--- src/package1/SuperTicTacToe.java
+++ src/package1/SuperTicTacToe.java
@@ -8,29 +8,6 @@ public class SuperTicTacToe {
         JFrame frame = new JFrame("Super Tic Tac Toe");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //phil pil = new startParametersPanel();
-        JTextField rowsField = new JTextField(2);
-        JTextField colsField = new JTextField(2);
-        JTextField inaRowField = new JTextField(2);
-        JTextField whosFirstField = new JTextField(1);
-
-        JPanel myPanel = new JPanel();
-        myPanel.add(new JLabel("Number of Rows:"));
-        myPanel.add(rowsField);
-
-        myPanel.add(new JLabel("Number of Columns:"));
-        myPanel.add(colsField);
-
-        myPanel.add(new JLabel("How Many Need to be in a Row to Win?:"));
-        myPanel.add(inaRowField);
-
-        myPanel.add(new JLabel("Who Goes First (Please enter X of O):"));
-        myPanel.add(whosFirstField);
-
-        int result = JOptionPane.showConfirmDialog(null, myPanel,
-                "Please Enter X and Y Values", JOptionPane.OK_CANCEL_OPTION);
-
-
         SuperTicTacToePanel panel = new SuperTicTacToePanel();
         frame.getContentPane().add(panel);
 
--- src/package1/SuperTicTacToePanel.java
+++ src/package1/SuperTicTacToePanel.java
@@ -19,11 +19,31 @@ public class SuperTicTacToePanel extends JPanel implements ActionListener{
     private SuperTicTacToeGame game;
 
     public SuperTicTacToePanel() {
+
+        JTextField rowsField = new JTextField(2);
+        JTextField colsField = new JTextField(2);
+        JTextField inaRowField = new JTextField(2);
+        JTextField whosFirstField = new JTextField(1);
+
+        JPanel myPanel = new JPanel();
+        myPanel.add(new JLabel("Number of Rows:"));
+        myPanel.add(rowsField);
+
+        myPanel.add(new JLabel("Number of Columns:"));
+        myPanel.add(colsField);
+
+        myPanel.add(new JLabel("How Many Need to be in a Row to Win?:"));
+        myPanel.add(inaRowField);
+
+        myPanel.add(new JLabel("Who Goes First (Please enter X of O):"));
+        myPanel.add(whosFirstField);
+
+        int result = JOptionPane.showConfirmDialog(null, myPanel,
+                "Please Enter X and Y Values", JOptionPane.OK_CANCEL_OPTION);
+
+
         xIcon = new ImageIcon("x.jpg");
         oIcon = new ImageIcon("o.jpg");
-
-        String startsFirst = JOptionPane.showInputDialog(this, "Who starts first? (Insert X or O)");
-        game.whoStartsFirst(startsFirst);
 
 
         quitButton = new JButton("Quit");
